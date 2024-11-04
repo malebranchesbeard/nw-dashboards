c4f5f1727dcb98e17e1eb523ca35f7d50416bba3
malebranchesbeard <gonzalo.g.fidalgo@gmail.com>
2024-11-04 15:09:33 +0100

    Improved feel and functionality of Visualisation in LLM workflow (seniority). Corrected apostrophe.

--- app/llm-workflow/components/dataComponent.js
+++ app/llm-workflow/components/dataComponent.js
@@ -16,7 +16,8 @@ const DataComponent = ({ candidateId }) => {
     <div className="grid grid-cols-3 gap-4 h-full overflow-auto">
       <div className="bg-gray-100 p-4 rounded-lg">
         <h3 className="text-lg font-semibold mb-4">
-          <Linkedin className="inline-block pb-1" /> {candidateName}'s Positions
+          <Linkedin className="inline-block pb-1" /> {candidateName}&apos;s
+          Positions
         </h3>
         <pre className="whitespace-pre-wrap text-sm">
           {JSON.stringify(candidate, null, 2)}
